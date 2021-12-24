4d43ccb897d070f13440e45690d4de0afaa75dad
prameell <95845757+prameell@users.noreply.github.com>
2021-12-24 20:18:58 +0530

    Add files via upload

---
+++ Core_java_Assignments/Assignment_3/ReverseStrBuffer.java
@@ -0,0 +1,15 @@
+
+public class ReverseStrBuffer {
+	
+		public static void main(String[] args)
+		{
+			StringBuffer str = new StringBuffer ("This method returns the reversed object on which it was called");
+
+			System.out.println("String =" + str);           
+			System.out.println(str.reverse());
+		}
+	}
+
+
+
+
