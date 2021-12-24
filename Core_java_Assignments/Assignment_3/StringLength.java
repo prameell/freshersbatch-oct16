18ec90f1eeed1394606215029dd72c5ccde5c296
prameell <95845757+prameell@users.noreply.github.com>
2021-12-24 17:12:32 +0530

    Add files via upload

---
+++ Core_java_Assignments/Assignment_3/StringLength.java
@@ -0,0 +1,11 @@
+
+public class StringLength {  
+	
+		public static void main(String args[]){  
+		
+			String s="Hello World";  
+	        System.out.println("string length is: "+s.length());
+
+		}
+		        }  
+
