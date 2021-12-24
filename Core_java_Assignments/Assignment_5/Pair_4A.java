a3de3d89b0d7707a9c3fd8b214472a1c45535ca5
prameell <95845757+prameell@users.noreply.github.com>
2021-12-24 16:52:08 +0530

    Add files via upload

---
+++ Core_java_Assignments/Assignment_5/Pair_4A.java
@@ -0,0 +1,16 @@
+package KeyValue4_A;
+
+import java.util.*;
+
+public class Pair_4A {
+	public static void main(String[] args) {
+		
+		HashMap<String,String> myObj=new HashMap<String,String>();
+		myObj.put("1","Hello");
+
+		System.out.println(myObj.keySet()+" "+myObj.values());
+
+
+		}
+
+}
