83054597da7c549e11116b7a96c392891be0f149
prameell <95845757+prameell@users.noreply.github.com>
2021-12-24 21:23:23 +0530

    Add files via upload

---
+++ Core_java_Assignments/Assignment_3/ApendStrBuilder.java
@@ -0,0 +1,12 @@
+
+public class ApendStrBuilder {
+	
+	public static void main(String[] args)
+	{
+
+		StringBuilder sb = new StringBuilder("String Buffer is a peer class of String");
+		sb.append("that provides much of");
+		sb.append("the functionality of strings");
+		System.out.println(sb);
+	 }
+     }
