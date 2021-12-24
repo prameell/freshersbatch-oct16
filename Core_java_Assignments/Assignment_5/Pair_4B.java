528ce5b586d3dc4716fc7d3f26a3de3f7fc388cf
prameell <95845757+prameell@users.noreply.github.com>
2021-12-24 16:52:49 +0530

    Add files via upload

---
+++ Core_java_Assignments/Assignment_5/Pair_4B.java
@@ -0,0 +1,17 @@
+package keyvalue4_B;
+
+import java.util.*;
+
+public class Pair_4B {
+	
+	public static void main(String[] args) 
+	    {
+		
+		HashMap<String,String> myObj=new HashMap<String,String>();
+		myObj.put("Today is","new java.util.Date()");
+
+		System.out.println(myObj.keySet()+" "+myObj.values());
+
+		}
+
+        }
