b9e6cf146d55278114bcdad1ab904ad78137ce87
prameell <95845757+prameell@users.noreply.github.com>
2021-12-24 17:12:54 +0530

    Add files via upload

---
+++ Core_java_Assignments/Assignment_3/StringMethods.java
@@ -0,0 +1,30 @@
+
+public class StringMethods {
+	
+   public static void main(String[] args) {
+	
+	String str="Java String pool refers to collection of Strings which are stored in heap memory";
+
+	System.out.println("String in lowercase "+str.toLowerCase());
+
+	System.out.println("String in upperCase "+str.toUpperCase());
+
+	System.out.println("String after replace "+str.replace('a','$'));
+
+	System.out.println("The word collection found "+str.contains("collection"));
+
+	String str1="java string pool refers to collection of strings which are stored in heap memory ";
+	System.out.println("Matches or not: "+str.matches(str1));
+
+	if(str==str1)
+	{
+		
+	System.out.println("two strings are matched");
+	}
+	else
+		
+	{ System.out.println("Two strings are not matched");
+	}
+	}
+    }
+
