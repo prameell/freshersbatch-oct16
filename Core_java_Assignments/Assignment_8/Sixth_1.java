c1eadcbaebc83ddb4cfd3e73fff31a4ee364d227
prameell <95845757+prameell@users.noreply.github.com>
2022-01-03 09:24:26 +0530

    Add files via upload

---
+++ Core_java_Assignments/Assignment_8/Sixth_1.java
@@ -0,0 +1,18 @@
+package Sixth;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Sixth_1 {
+	public static void main(String[] args) throws CloneNotSupportedException 
+    {
+        ArrayList<String> alphabets = new ArrayList<>(Arrays.asList("deepika", "manoj", "teja", "aruna", "sweety"));
+         
+        System.out.println(alphabets);
+         
+        alphabets.replaceAll( e -> e.toUpperCase() );  
+         
+        System.out.println(alphabets);
+    }
+
+}
---
+++ Core_java_Assignments/Assignment_8/Sixth_2.java
@@ -0,0 +1,27 @@
+package Sixth;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.UnaryOperator;
+
+public class Sixth_2 {
+	public static void main(String[] args) throws CloneNotSupportedException 
+    {
+        ArrayList<String> alphabets = new ArrayList<>(Arrays.asList("deepika", "manoj", "teja", "aruna", "sweety"));
+         
+        System.out.println(alphabets);
+         
+        alphabets.replaceAll( new MyOperator() );  
+         
+        System.out.println(alphabets);
+    }
+}
+ 
+class MyOperator implements UnaryOperator<String> 
+{
+    @Override
+    public String apply(String t) {
+        return t.toUpperCase();
+    }
+
+}
