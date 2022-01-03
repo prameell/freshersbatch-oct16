f5ea787b1f4936b7dff9964cb84f805deca7fb77
prameell <95845757+prameell@users.noreply.github.com>
2022-01-03 08:54:27 +0530

    Add files via upload

---
+++ Core_java_Assignments/Assignment_6/Hashset.java
@@ -0,0 +1,19 @@
+package Collections2product;
+import java.util.TreeSet;
+
+public class Hashset {
+	public static void main(String[] args) {
+		TreeSet<String> set = new TreeSet<String>();
+		set.add("Rajeshwari");
+		set.add("Venkat");
+		set.add("Rajesh");
+		set.add("Madhuri");
+		set.add("Kumari");
+		set.add("Narayan");
+		set.add("Koti");
+		set.add("Rajesh");
+		set.add("Aruna");
+		System.out.println("Set is :" +set);
+	}
+
+}
